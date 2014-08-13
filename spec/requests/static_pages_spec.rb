7aad1f8c20d8247a7c375ea8d1f9cc49ac64a1ec
Peter Chambers <peter.j.chambers@gmail.com>
2014-08-13 13:33:35 -0400

    Finish static pages

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,18 +2,43 @@ require 'spec_helper'
 
 describe "Static pages" do
   
-  describe "Home page" do
+  describe "Help page" do
     
-	it "should have the content 'Sample App'" do
-      visit '/static_pages/home'
-      expect(page).to have_content('Sample App')
+	it "should have the right title" do
+	  visit '/static_pages/help'
+	  expect(page).to have_title("Ruby on Rails Tutorial Sample App | Help")
+	end
+	
+	it "should have the content 'Help'" do
+      visit '/static_pages/help'
+      expect(page).to have_content('Help')
     end
   end
 
-  # describe "Help page" do
-	# it "should have the content 'Help'" do
-	  # visit 'static_pages/help'
-	  # expect(page).to have_content('Help')
-	# end
-  # end
+   describe "Home page" do
+	
+	it "should have the right title" do
+	  visit '/static_pages/home'
+	  expect(page).to have_title("Ruby on Rails Tutorial Sample App | Home")
+	end
+	
+	it "should have the content 'Sample App'" do
+	   visit '/static_pages/home'
+	   expect(page).to have_content('Sample App')
+	 end
+   end
+
+   describe "About page" do
+	
+	it "should have the right title" do
+	  visit '/static_pages/about'
+	  expect(page).to have_title("Ruby on Rails Tutorial Sample App | About")
+	end
+	
+	it "should have the content 'About'" do
+	   visit '/static_pages/about'
+	   expect(page).to have_content('About')
+    end
+   end
+   
 end
