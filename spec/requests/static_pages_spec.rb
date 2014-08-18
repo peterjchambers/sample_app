533a0f5896ffb3a94044a09a3fc32b9e3eca7270
Peter Chambers <peter.j.chambers@gmail.com>
2014-08-18 08:12:11 -0400

    Finish contact page

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,11 +2,26 @@ require 'spec_helper'
 
 describe "Static pages" do
   
+  let (:base_title) { "Ruby on Rails Tutorial Sample App" }
+  
+  describe "Contact page" do
+
+    it "should have the content 'Contact'" do
+      visit '/static_pages/contact'
+      expect(page).to have_content('Contact')
+    end
+
+    it "should have the title 'Contact'" do
+      visit '/static_pages/contact'
+      expect(page).to have_title( "#{base_title} | Contact")
+    end
+  end
+  
   describe "Help page" do
     
 	it "should have the right title" do
 	  visit '/static_pages/help'
-	  expect(page).to have_title("Ruby on Rails Tutorial Sample App | Help")
+	  expect(page).to have_title("#{base_title} | Help")
 	end
 	
 	it "should have the content 'Help'" do
@@ -19,7 +34,7 @@ describe "Static pages" do
 	
 	it "should have the right title" do
 	  visit '/static_pages/home'
-	  expect(page).to have_title("Ruby on Rails Tutorial Sample App | Home")
+	  expect(page).to have_title("#{base_title} | Home")
 	end
 	
 	it "should have the content 'Sample App'" do
@@ -32,7 +47,7 @@ describe "Static pages" do
 	
 	it "should have the right title" do
 	  visit '/static_pages/about'
-	  expect(page).to have_title("Ruby on Rails Tutorial Sample App | About")
+	  expect(page).to have_title("#{base_title} | About")
 	end
 	
 	it "should have the content 'About'" do
